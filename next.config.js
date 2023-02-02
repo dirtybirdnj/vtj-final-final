8f1ec8583d27f47aea25de0d57850b4efc3fa4df
Nicole Colabella <>
2023-02-02 18:18:09 -0500

    fix header/footer issue

--- next.config.js
+++ next.config.js
@@ -1,25 +1,13 @@
 const fs = require("fs").promises;
 
-const pages = fs.readdir(__dirname + "/src/pages/").then(console.log);
-
-// This is annoyingly not working
-// const getPages = async () => {
-//   const pages = await fs.readdir(__dirname + "/src/pages/");
-//   console.log('pages');
-
-//   return pages
-// }
-
-// const pages = getPages();
-// console.log('pages', pages);
+fs.readdir(__dirname + "/src/pages/").then(console.log);
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
 
 module.exports = withMDX({
-  pageExtensions: ['md', 'mdx'],
-  publicRuntimeConfig: {
-    pages: pages
-  } 
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 })
+
+
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,9 @@
-import { useState, useEffect } from 'react';
 import Layout from '../components/layout';
 import '../styles/globals.css';
 import getConfig from 'next/config'
 
 function App({ Component, pageProps }) {
-  const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
-  const [pagesDir, setPagesDir] = useState(null);
-
-  console.log('publicRuntimeConfig', publicRuntimeConfig);
-
-  useEffect(() => {
-    if (publicRuntimeConfig) {
-      setPagesDir(publicRuntimeConfig);
-    }
-  }, [publicRuntimeConfig]);
-
-  useEffect(() => {
-    if (pagesDir)
-      console.log('pagesDir', pagesDir);
-  }, [pagesDir])
-
+  console.log('getConfig', getConfig());
   return (
     <Layout>
       <Component {...pageProps} />
