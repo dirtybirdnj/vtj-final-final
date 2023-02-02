cb732e1a309b035c33dc972f88e36791dc09ce74
Nicole Colabella <>
2023-02-02 15:22:02 -0500

    getting file names in console

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const fs = require("fs").promises;
+
+fs.readdir(__dirname + "/src/pages/").then(console.log);
+
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 })
+
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 })
+
+
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
 import Layout from '../components/layout';
 import '../styles/globals.css';
+import getConfig from 'next/config'
+
 function App({ Component, pageProps }) {
+  console.log('getConfig', getConfig());
   return (
     <Layout>
       <Component {...pageProps} />
