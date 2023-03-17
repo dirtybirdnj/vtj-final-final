be666af0cc7053c349e4c999b256fdbe94cf2483
Nicole Colabella <>
2023-03-17 14:39:50 -0400

    footer styled

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,54 +1,54 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
+import logoImage from '../../public/vtj-circle.svg';
+
+const FooterEl = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 50px 0 3%;
+
+  @media only screen and (max-width: 559px) {
+    padding: 50px 0 5%;
+  }
+`;
+
+const Logo = styled(Image)`
+  height: 8vh;
+  width: auto;
+  cursor: pointer;
+`;
+
+const RightSide = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: flex-end;
+  gap: 8px;
+  font-size: 14px;
+  padding-bottom: 8px;
+
+  a {
+    color: darkblue;
+    font-size: 15px;
+  }
+`;
 
 function Footer({
   pageData,
   currentRoute
 }) {
 
-  const links = pageData.map((page, i) => {
-    //console.log('pageData', pageData);
-
-    if (page.path && (page.data.nav || page.data.title)) {
-      const pagePath = page.path;
-      const pageTitle = page.data.title;
-      const navName = page.data.nav;
-      const pageOrder = page.data.navOrder ? page.data.navOrder : 100;
-      const linkStyles = {
-        display: 'flex',
-        order: pageOrder
-      }
-
-      return <Link style={linkStyles} key={i} className={currentRoute === page.path ? 'active' : ''} href={pagePath}>{navName ? navName : pageTitle}</Link>
-    } else {
-      console.log('missing page props', page);
-    }
-
-  })
-
-  const imgStyle = {
-    maxWidth: '100px'
-  }
-
-  const containerStyles = {
-
-    margin: '0px auto',
-    display: 'flex',
-    flexDirection: 'column',
-    overflow: 'hidden',
-
-  }
-
   return (
-    <footer>
-      <div style={containerStyles}>
-        <Image style={imgStyle} src="/vtj-circle.svg" width="300" height="300" alt="verticaltubejig.com logo"/>
-        <p>www.verticaltubejig.com</p>
-        <p>orders@verticaltubejig.com</p>
-        <p >EST. 2023 Huntington, VT</p>
-        <p>vtapi.co</p>
-      </div>
-    </footer>
+    <FooterEl>
+      <Logo onClick={() => router.push('/')} priority src={logoImage} alt="verticaltubejig.com logo" />
+      <RightSide>
+        <Link href="mailto: orders@verticaltubejig.com">orders@verticaltubejig.com</Link>
+        <span>EST. 2023 Huntington, VT | <strong>vtapi.co</strong></span>
+      </RightSide>
+    </FooterEl>
   )
 }
 
