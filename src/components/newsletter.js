a499fd4603e1cecaab3ad7fe8996bf16a88f31a9
Nicole Colabella <>
2023-03-17 15:32:32 -0400

    Update newsletter styles

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -1,43 +1,38 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import { ButtondownForm } from 'react-buttondown'
-import 'react-buttondown/dist/react-buttondown.css'
+import styled from 'styled-components';
+import { ButtondownForm } from 'react-buttondown';
+import 'react-buttondown/dist/react-buttondown.css';
+
+const NewsletterContainer = styled.div`
+  h3 {
+    margin-bottom: 10px;
+  }
+
+  button {
+    background: darkblue;
+    align-self: stretch;
+  }
+`;
 
 function Newsletter({
   pageData,
   currentRoute
-}) {
-
-  const imgStyle = {
-    maxWidth: '100px'
-  }
-
-  const containerStyles = {
-
-    margin: '0px auto',
-    display: 'flex',
-    flexDirection: 'column',
-    overflow: 'hidden',
-
-  }
-  
+}) {  
   const handleOnSubscribe = subscriber => {
-
     //new BD subscriber
     console.log('new sub',subscriber);
-
    }
 
   return (
-
-      <div style={containerStyles}>
-        <h3>Sign up for our newsletter</h3>
-        <p>and get the DROP on the next batch of lures to go on sale!</p>
-        <ButtondownForm
-          apiKey={process.env.BD_API_KEY}
-          onSubscribe={handleOnSubscribe}
-        />
-      </div>
+    <NewsletterContainer>
+      <h3>Sign up for our newsletter</h3>
+      <p>Get the DROP on the next batch of lures to go on sale!</p>
+      <ButtondownForm
+        apiKey={process.env.BD_API_KEY}
+        onSubscribe={handleOnSubscribe}
+      />
+    </NewsletterContainer>
   )
 }
 
