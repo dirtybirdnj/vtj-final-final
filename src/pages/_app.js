78e5b209cce1d1760016afed6be1f07a1936e8d8
Mat Gilbert <matgilbert@gmail.com>
2023-05-13 23:12:05 -0400

    Added next.js analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout';
 import '../styles/globals.css';
 import Script from 'next/script';
 import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }) {
 
@@ -21,6 +22,7 @@ function App({ Component, pageProps }) {
         hidden
       ></div>
       <Script src="https://cdn.snipcart.com/themes/v3.4.1/default/snipcart.js" />
+      <Analytics />
     </>
   );
 }
